c933030feb150a1463125edb8ad0f8bb5adf9124
Joel Carlson <joelcarlsonesl@gmail.com>
2015-08-20 18:32:10 +0900

    Update FirstOrder.R

--- R/FirstOrder.R
+++ R/FirstOrder.R
@@ -10,15 +10,15 @@ NULL
 #' 
 calc_energy <- function(image, dims=NULL){
   #TODO: Add dim check for 2D vs 3D
-  return(sum(image*image))
+  return(sum(image*image, na.rm=TRUE))
 }
 
 #' @describeIn first_order_features Entropy
 
-calc_entropy <- function(image, base=2, nbins=length(image)){
+calc_entropy <- function(image, base=2, nbins=length(image[!is.na(image)])){
   # Break image into a hist
-  im_range <- range(image, na.rm=TRUE)
-  cuts <- table(cut(image, seq(im_range[1], im_range[2], by=diff(im_range)/nbins), include.lowest=TRUE))/length(image)
+    im_range <- range(image, na.rm=TRUE)
+  cuts <- table(cut(image, seq(im_range[1], im_range[2], by=diff(im_range)/nbins), include.lowest=TRUE))/length(image[!is.na(image)])
   
   #Logs cannot take 0 values, so let = 0 if no value
   entropy_vals <- vapply(cuts, function(x) ifelse(x != 0, x*logb(x, base=base), 0), FUN.VALUE = 1)
@@ -27,8 +27,8 @@ calc_entropy <- function(image, base=2, nbins=length(image)){
 
 calc_kurtosis <- function(image){
   scale <- 1/prod(dim(image))
-  mu <- mean(image)
-  sigma <- sd(image)
+  mu <- mean(image, na.rm=TRUE)
+  sigma <- sd(image, na.rm=TRUE)
   
   function_val <- vapply(image, function(x) ((x - mu)/sigma)^4, FUN.VALUE=1)
   return(scale * sum(function_val) - 3)
@@ -38,7 +38,7 @@ calc_kurtosis <- function(image){
 #' 
 calc_kurtosisOptimized <- function(image){
   n <- length(image)
-  image <- image - mean(image)
+  image <- image - mean(image, na.rm=TRUE)
   r <- n * sum(image^4) / (sum(image^2)^2)
   return(r * (1 - 1/n)^2 - 3)
 }
