93df811478e6c84b0298d29245951f8883eee14f
Joel Carlson <joelcarlsonesl@gmail.com>
2015-11-24 20:35:15 +0900

    Add truncate option to glrlm

--- R/GLRLM.R
+++ R/GLRLM.R
@@ -7,13 +7,14 @@
 #' see \code{?image.radiomics}
 #' 
 #' @param data A numeric 2D matrix.
-#' @param angle One of "0", "45", "90" or "135", the direction the run is calculated.
+#' @param angle One of 0, 45, 90 or 135, the direction the run is calculated.
 #' @param n_grey an integer value, the number of grey levels the image should
 #'   be quantized into.
 #' @param max_run_length An integer value, the default is the maximum possible
 #'   run length. Setting it to a smaller value truncates the output. Desirable
 #'   in cases where the matrix is extremely sparse, for example when
 #'   there are few long runs.
+#' @param truncate Logical Remove run lengths which have no entries 
 #' @param ... Can be given verbose=FALSE to suppress output from the n_grey conversion.       
 #' @return a matrix of class "glrlm" of dimension n_grey by run length. The column 
 #'   names represent the length of the run, and row names represent 
@@ -34,7 +35,7 @@ glrlm <- setClass("glrlm",
 
 setMethod("initialize", 
           signature = "glrlm", 
-          definition = function(.Object, data, angle, n_grey, max_run_length, ...){
+          definition = function(.Object, data, angle, n_grey, max_run_length, truncate, ...){
             
             #send to discretizeImage for error checking
             data <- discretizeImage(data, n_grey=n_grey, ...)
@@ -73,6 +74,7 @@ setMethod("initialize",
               stop("Angle must be one of '0', '45', '90', '135'.")
             }
             
+            if(truncate) rlm <- rlm[,which(colSums(rlm) > 0)]
             
             .Object@.Data <- rlm
             
@@ -83,7 +85,7 @@ setMethod("initialize",
 )
 
 #' @export          
-glrlm <- function(data, angle = 0, n_grey = 32, max_run_length = min(dim(data)), ...){
+glrlm <- function(data, angle = 0, n_grey = 32, max_run_length = min(dim(data)), truncate=TRUE, ...){
   return(new("glrlm", data, angle, n_grey, max_run_length, ...))
 }
 
